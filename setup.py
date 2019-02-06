695bbce203627c378d8cd621ea201603b5bcd78d
William Falcon <waf2107@columbia.edu>
2019-02-06 17:16:14 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='pytorch-complex-tensor',
+      version='0.0.1',
+      description='Pytorch complex tensor',
+      author='',
+      author_email='',
+      url='https://github.com/williamFalcon/pytorch-complex-tensor',
+      install_requires=[],
+      packages=find_packages()
+      )
