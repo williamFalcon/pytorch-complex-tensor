d49a07dfa8fa9da2ba763f4ce2f6198bd8d0adfd
William Falcon <waf2107@columbia.edu>
2019-02-08 18:41:08 -0500

    release v0.0.131

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
       name='pytorch-complex-tensor',
-      version='0.0.130',
+      version='0.0.131',
       description='Pytorch complex tensor',
       author='William Falcon',
       author_email='waf2107@columbia.edu',
